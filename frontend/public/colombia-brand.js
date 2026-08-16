06b240497b3333fa634eb922bde3af6aa5bf9809
ColombiaVasilevAlexandr <158317479+ColombiaVasilevAlexandr@users.noreply.github.com>
2026-08-16 10:33:47 +0500

    Show Colombia flag and Russian exam title

--- frontend/public/colombia-brand.js
+++ frontend/public/colombia-brand.js
@@ -3,7 +3,7 @@
     const brand = document.querySelector('.sidebar .brand')
     if (!brand || brand.dataset.colombiaBrand) return
     brand.dataset.colombiaBrand = 'true'
-    brand.innerHTML = '<span class="colombia-flag" aria-label="Флаг Колумбии"><i></i><i></i><i></i></span><b>EXAMEN<br>DE COLOMBIA</b>'
+    brand.innerHTML = '<span class="colombia-flag" aria-label="Флаг Колумбии"><i></i><i></i><i></i></span><b>КОЛУМБИЯ<br>ЭКЗАМЕН</b>'
   }
   apply(); new MutationObserver(apply).observe(document.documentElement, {childList:true,subtree:true})
 })()
